ac25e98e140325d5e68517a738d62a373c598e7f
JFDBear1@gmail.com <JFDBear1@gmail.com@0b8b4a0d-f7ff-6bb2-686f-e5fdcdd9e42b>
2012-07-02 03:59:27 +0000

    Added tileSpec.h to the project to outline the next phase of the drunken walk. Progress on Fractal work is slow at best (might abandon in the current scope to prevent scope creep).

---
+++ Maptals/Maptals/tileSpec.h
@@ -0,0 +1,20 @@
+/*!
+* \author John Dunham
+* \date 7-1-12
+* 
+* This is a class designed to specify a set of rules for a tile in a tile based game, indicating valid tiles that follow it in the 4 cardinal directions.
+* This class should not be handled by any user of this library.
+*
+*/
+#ifndef MAPTALS_TILE_SPEC_H
+#define MAPTALS_TILE_SPEC_H
+#endif
+
+// TODO
+// Tile Value.
+// Valid directions.
+// Variable tile rarity.
+// References to other tiles.
+// Randomizer for next tile (e.g. getNextTile(int dir)).
+// Design a file format for storing alongside tile.
+// 
