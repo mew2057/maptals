65a24456f42532eec2a8ffaa48f62df391f4169b
JFDBear1@gmail.com <JFDBear1@gmail.com@0b8b4a0d-f7ff-6bb2-686f-e5fdcdd9e42b>
2012-07-02 21:58:11 +0000

    Added som randomizing logic to tileSpec, began work on the tile map.

--- Maptals/Maptals/TileSpec.cpp
+++ Maptals/Maptals/TileSpec.cpp
@@ -15,22 +15,25 @@ int TileSpec::getNextTile(int direction){
     return getNextTile(direction, defaultSeed);
 }
 
+//! This is evenly random, no weight on outcomes.
 int TileSpec::getNextTile(int direction, unsigned int seed){
     srand(seed);
 
-    
-    return 0;
+    if(direction < succeedingTiles.size())
+        return rand()%succeedingTiles.at(direction).size();
+    else
+        return -1;
 }
 
 void TileSpec::appendTile(int direction, int tile){
-    succeedingTiles.at(direction).push_back(tile);
+    if(direction < succeedingTiles.size())
+        succeedingTiles.at(direction).push_back(tile);
+    //TODO add error condition.    
 }
 
 TileSpec::TileSpec(){
     succeedingTiles = std::vector<std::vector<int>>(4, std::vector<int>(0));
     tileValue = -1;
-
-
 }
 
 TileSpec::~TileSpec(){
